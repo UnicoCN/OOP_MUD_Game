c84fdde62e7760f5439578815b0bc846c3b8d7ad
胡嘉成 <3200103738@zju.edu.cn>
2022-05-03 15:14:47 +0800

    first_push

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include <Windows.h>
+using namespace std;
+
+class Map {
+
+    void Print_ (int num)
+
+};
+
+class Floor : public Map {
+    string Floor[10] = "d  d s e  d d"
+
+}
+
+void Echo_Static() {
+    cout << "属性:" << endl;
+    cout << "当前所在楼层：" << endl;
+    cout << "物品" << endl;
+}
+
+void Echo_Map() {
+    cout << "---------------------------" << endl;
+    cout << "    门        人     楼梯  " << endl;
+    cout << "---------------------------" << endl;
+}
+
+void Echo_Map2() {
+    cout << "---------------------------" << endl;
+    cout << "    怪        人     楼梯  " << endl;
+    cout << "---------------------------" << endl;
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    Echo_Map();
+    getchar();
+    system("cls");
+    Echo_Map2();
+    // system("pause");
+    while (1);
+    return 0;
+}
